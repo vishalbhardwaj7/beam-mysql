5696bdc9d79083e48619cb384ed3a6f93418a934
Kirill Boltaev <aske@fmap.me>
2020-09-09 21:49:50 +0300

    Add bobby tables test (wip)

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeFamilies   #-}
 
 module Main (main) where
@@ -19,6 +20,13 @@ import           Database.MySQL.Base (MySQLConn, Query (Query), close, connect,
 import           Database.MySQL.Temp (MySQLDB, toConnectInfo, withTempDB)
 import           GHC.Generics (Generic)
 import           Test.Hspec (Spec, describe, hspec, it, shouldBe)
+
+import           Data.ByteString as ByteString
+import qualified Data.Text.Lazy as TL
+
+import qualified Data.ByteString.Lazy as BSL
+
+import qualified Database.Beam as B
 
 main :: IO ()
 main = do
@@ -45,118 +53,52 @@ setUpDB conn = traverse_ (execute_ conn) [
     makeCorrect = Query $
       "create table correct_table (" <>
       "id bigint primary key auto_increment, " <>
-      "int8_varchar varchar(255) not null, " <>
-      "int16_varchar varchar(255) not null, " <>
-      "int32_varchar varchar(255) not null, " <>
-      "int64_varchar varchar(255) not null, " <>
-      "float_varchar varchar(255) not null, " <>
-      "double_varchar varchar(255) not null, " <>
-      "text_tinyint tinyint not null, " <>
-      "text_smallint smallint not null, " <>
-      "text_int int not null, " <>
-      "text_bigint bigint not null, " <>
-      "text_float float not null, " <>
-      "int8_float float not null, " <>
-      "int16_float float not null, " <>
-      "int32_float float not null, " <>
-      "int64_float float not null, " <>
-      "text_double double not null, " <>
-      "int8_double double not null, " <>
-      "int16_double double not null, " <>
-      "int32_double double not null, " <>
-      "int64_double double not null" <>
+      "bad_text varchar(255) not null, " <>
+      "bad_text2 text not null" <>
       ");"
     insertCorrect :: Query
     insertCorrect = Query $
       "insert into correct_table (" <>
-      "id, int8_varchar, int16_varchar, int32_varchar, int64_varchar, " <>
-      "float_varchar, double_varchar, " <>
-      "text_tinyint, text_smallint, text_int, text_bigint, " <>
-      "text_float, int8_float, int16_float, int32_float, int64_float, " <>
-      "text_double, int8_double, int16_double, int32_double, int64_double) " <>
+      "id, bad_text, bad_text2)"  <>
       "values (" <>
-      "DEFAULT, '10', '256', '65666', '5000000000', '10.05', '10.0500505005', " <>
-      "10, 256, 65666, 5000000000, " <>
-      "10.05, 10.05, 10.05, 10.05, 10.05, " <>
-      "10.0500505005, 10.0500505005, 10.0500505005, 10.0500505005, 10.0500505005);"
+      "DEFAULT," <>
+      "\'foo\\'\'," <>
+      bobbyTablesBS <>
+      ")"
 
 runCorrectQuery :: MySQLConn -> IO (CorrectT Identity)
 runCorrectQuery conn = do
-  res <- runBeamMySQL conn . runSelectReturningOne $ query
+  res <- runBeamMySQL conn . B.runSelectReturningList $ query
   case res of
-    Nothing   -> fail "Expected query result, but got nothing."
-    Just res' -> pure res'
+    []   -> fail "Expected query result, but got nothing."
+    res':_ -> do
+      -- print res
+      print $ _correctBadText res'
+      print $ _correctBadText2 res'
+      pure $ res'
   where
     query :: SqlSelect MySQL (CorrectT Identity)
-    query = select . all_ . _testCorrectTable $ testDB
+    query = select . B.limit_ 1 . B.filter_' predicate $ B.all_ (_testCorrectTable testDB)
+      where predicate CorrectT {_correctBadText2} =
+              _correctBadText2 B.==?. B.val_ bobbyTables
+
+bobbyTables :: Text
+bobbyTables = "\'; DROP TABLE students; --"
+
+bobbyTablesBS :: BSL.ByteString
+bobbyTablesBS = "\'\\'; DROP TABLE students; --\'"
 
 spec :: CorrectT Identity -> Spec
 spec res = do
-  describe "Lenient parsing (success)" $ do
-    it "should parse Int8 from VARCHAR" $
-      _correctInt8Varchar res `shouldBe` 10
-    it "should parse Int16 from VARCHAR" $
-      _correctInt16Varchar res `shouldBe` 256
-    it "should parse Int32 from VARCHAR" $
-      _correctInt32Varchar res `shouldBe` 65666
-    it "should parse Int64 from VARCHAR" $
-      _correctInt64Varchar res `shouldBe` 5000000000
-    it "should parse Float from VARCHAR" $
-      _correctFloatVarchar res `shouldBe` 10.05 -- TODO: Proper float equality. - Koz
-    it "should parse Double from VARCHAR" $
-      _correctDoubleVarchar res `shouldBe` 10.0500505005
-    it "should parse Text from TINYINT" $
-      _correctTextTinyint res `shouldBe` "10"
-    it "should parse Text from SMALLINT" $
-      _correctTextSmallint res `shouldBe` "256"
-    it "should parse Text from INT" $
-      _correctTextInt res `shouldBe` "65666"
-    it "should parse Text from BIGINT" $
-      _correctTextBigint res `shouldBe` "5000000000"
-    it "should parse Text from FLOAT" $
-      _correctTextFloat res `shouldBe` "10.05"
-    it "should parse Int8 from FLOAT" $
-      _correctInt8Float res `shouldBe` 10
-    it "should parse Int16 from FLOAT" $
-      _correctInt16Float res `shouldBe` 10
-    it "should parse Int32 from FLOAT" $
-      _correctInt32Float res `shouldBe` 10
-    it "should parse Int64 from FLOAT" $
-      _correctInt64Float res `shouldBe` 10
-    it "should parse Text from DOUBLE" $
-      _correctTextDouble res `shouldBe` "10.0500505005"
-    it "should parse Int8 from DOUBLE" $
-      _correctInt8Double res `shouldBe` 10
-    it "should parse Int16 from DOUBLE" $
-      _correctInt16Double res `shouldBe` 10
-    it "should parse Int32 from DOUBLE" $
-      _correctInt32Double res `shouldBe` 10
-    it "should parse Int64 from DOUBLE" $
-      _correctInt64Double res `shouldBe` 10
+  describe "Escaping tests" $ do
+    it "shouldn't allow little Bobby Tables to drop students table" $ do
+      _correctBadText2 res `shouldBe` bobbyTables
 
 data CorrectT (f :: Type -> Type) = CorrectT
   {
-    _correctId            :: Columnar f Int64,
-    _correctInt8Varchar   :: Columnar f Int8,
-    _correctInt16Varchar  :: Columnar f Int16,
-    _correctInt32Varchar  :: Columnar f Int32,
-    _correctInt64Varchar  :: Columnar f Int64,
-    _correctFloatVarchar  :: Columnar f Float,
-    _correctDoubleVarchar :: Columnar f Double,
-    _correctTextTinyint   :: Columnar f Text,
-    _correctTextSmallint  :: Columnar f Text,
-    _correctTextInt       :: Columnar f Text,
-    _correctTextBigint    :: Columnar f Text,
-    _correctTextFloat     :: Columnar f Text,
-    _correctInt8Float     :: Columnar f Int8,
-    _correctInt16Float    :: Columnar f Int16,
-    _correctInt32Float    :: Columnar f Int32,
-    _correctInt64Float    :: Columnar f Int64,
-    _correctTextDouble    :: Columnar f Text,
-    _correctInt8Double    :: Columnar f Int8,
-    _correctInt16Double   :: Columnar f Int16,
-    _correctInt32Double   :: Columnar f Int32,
-    _correctInt64Double   :: Columnar f Int64
+    _correctId       :: Columnar f Int64,
+    _correctBadText  :: Columnar f Text,
+    _correctBadText2 :: Columnar f Text
   }
   deriving stock (Generic)
   deriving anyclass (Beamable)
