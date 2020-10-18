b7e60fb608a665328e8e6ccfab4a284883314134
Ben Hart <Benjmhart@gmail.com>
2020-10-18 15:23:00 -0400

    derive generic

--- src/Data/ViaJson.hs
+++ src/Data/ViaJson.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Data.ViaJson where
 
 import           Data.Aeson (FromJSON, ToJSON)
@@ -5,6 +7,10 @@ import           Data.Hashable (Hashable)
 import           Data.Kind (Type)
 import           Type.Reflection (Typeable)
 
+import Generics.SOP.TH
+
 newtype ViaJson (a :: Type) = ViaJson a
   deriving newtype (Eq, Ord, FromJSON, ToJSON, Hashable)
   deriving stock (Show, Typeable)
+
+deriveGeneric ''ViaJson
